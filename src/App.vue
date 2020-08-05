5c682c379d3c598a76416146c2ece03f51b9af04
wc-takahara <42595205+wc-takahara@users.noreply.github.com>
2020-08-05 16:48:27 +0900

    完成型

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,59 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Todo List</h1>
+    <span>作業名：</span><input v-model="task" type="text"><button id="add" v-on:click="addTodo">追加</button>
+    <todo-list v-on:remove="removeTodo" v-bind:items=todos></todo-list>
   </div>
 </template>
-
+ 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TodoList from './components/TodoList.vue'
+ 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    TodoList
+  },
+  data: function() {
+    return {
+      task: "",
+      todos: [],
+      count: 0,
+    }
+  },
+  methods: {
+    addTodo: function() {
+      if (this.task === "") {
+        alert("作業名を入力してください");
+        return;
+      }
+      //入力したタスク名を
+      this.todos.push({
+        message: this.task, id: ++this.count
+      });
+      //タスク名の初期化
+      this.task = "";
+    },
+    removeTodo: function(event, index) {
+      //タスクを削除
+      this.todos.splice(index, 1);
+    }
+  },
+  mounted: function() {
+    this.todos = JSON.parse(localStorage.getItem("todos")) || [];
+    // this.todos = [];
+    const todos = this.todos;
+    this.count = this.todos.length;
+    window.onbeforeunload = function() {
+      localStorage.setItem("todos", JSON.stringify(todos));
+    }
   }
 }
 </script>
-
+ 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/TodoList.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <ul id="todolist">
+      <li v-for="(item, index) in items" v-bind:key="item.id">
+        {{ item.message }}<button id="remove-button" @click="$emit('remove', $event, index)">削除</button>
+      </li>
+    </ul>
+  </div>
+</template>
+ 
+<script>
+export default {
+  name: 'todo-list',
+  props: {  
+    items: {
+      type: Array,
+      default: function() {
+        return [];
+      }
+    },
+  },
+}
+</script>
+ 
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  /* list-style-type: none; */
+  text-align: left;
+  padding: 0;
+  width: 100px;
+  white-space: nowrap;
+  margin: auto;
+}
+li {
+  display: list-item;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
